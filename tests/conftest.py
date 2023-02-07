ca8d18c8937b2346d7794aca19b727d0167aed0f
lykov-ivan-02 <lykov-ivan-02@yandex.ru>
2023-02-07 09:49:09 +0300

    fix ci

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,27 @@
-pytest_plugins = ['pytest_userver.plugins.core']
+import pathlib
+
+import pytest
+
+
+import json
+
+
+pytest_plugins = ['pytest_userver.plugins.redis']
+# /// [redis setup]
+
+
+# /// [service_env]
+@pytest.fixture(scope='session')
+def service_env(redis_sentinels):
+    secdist_config = {
+        'redis_settings': {
+            'taxi-tmp': {
+                'password': '',
+                'sentinels': redis_sentinels,
+                'shards': [{'name': 'mymaster'}],
+            },
+        },
+    }
+
+    return {'SECDIST_CONFIG': json.dumps(secdist_config)}
+    # /// [service_env]
